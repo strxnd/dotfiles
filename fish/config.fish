87d548d91312dbc7774436a42325510e131c5647
Kumar Aarav <kumar.aarav0609@gmail.com>
2025-03-08 13:58:15 +0800

    Add hyprlock and edit keybinds for neovim

--- fish/config.fish
+++ fish/config.fish
@@ -1,13 +1,15 @@
 if status is-interactive
-    # Run if shell is interactive
+  # Run if shell is interactive 
+  neofetch
 end
 
 # Aliases
 alias c="clear"
 alias ls="exa --icons --group-directories-first"
 alias cat="bat --style=numbers"
+alias cd="z"
 
 # Remove welcome message
 set fish_greeting
 
-neofetch
+zoxide init fish | source
--- nvim/lua/opts/keybinds.lua
+++ nvim/lua/opts/keybinds.lua
@@ -1,7 +1,2 @@
-vim.keymap.set({ "n", "i" }, "<C-s>", function()
-  vim.cmd("w")         -- Save file
-  vim.lsp.buf.format() -- Format file
-end, { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "View code actions" })
 vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, { desc = "Format current buffer" })
-vim.keymap.set("n", "<leader>ac", ":CopilotChatToggle<CR>", { desc = "Toggle copilot chat" })
