77d5b2a2bbe63ff4fe80e2d73679322aa43b432c
Kumar Aarav <kumar.aarav0609@gmail.com>
2025-04-04 15:24:28 +0800

    Remove autocommands.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,3 @@
 require("lazy.lazy")
 require("opts.vim-options")
 require("opts.keybinds")
-require("opts.autocommands")
--- nvim/lua/opts/autocommands.lua
+++
@@ -1,6 +0,0 @@
--- format on save
-vim.api.nvim_create_autocmd("BufWritePre", {
-	callback = function(args)
-		vim.lsp.buf.format({ async = false, bufnr = args.buf })
-	end,
-})
