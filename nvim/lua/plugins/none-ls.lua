7d51c56e72b0d78f1f3f14881b40a289cc74da66
Kumar Aarav <kumar.aarav0609@gmail.com>
2025-04-05 14:03:16 +0800

    Update waybar icons for bluetooth

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -3,6 +3,8 @@ return {
   config = function()
     local null_ls = require("null-ls")
 
+    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.stylua,
@@ -10,7 +12,22 @@ return {
         require("none-ls.diagnostics.eslint"),
         null_ls.builtins.formatting.clang_format,
       },
+      -- you can reuse a shared lspconfig on_attach callback here
+      on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            group = augroup,
+            buffer = bufnr,
+            callback = function()
+              -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+              -- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
+              vim.lsp.buf.format({ async = false })
+            end,
+          })
+        end
+      end,
     })
   end,
-  dependencies = { "nvim-lua/plenary.nvim", "nvimtools/none-ls-extras.nvim" }
+  dependencies = { "nvim-lua/plenary.nvim", "nvimtools/none-ls-extras.nvim" },
 }
--- waybar/scripts/bluetooth.sh
+++ waybar/scripts/bluetooth.sh
@@ -3,15 +3,15 @@
 # Get Bluetooth status
 powered=$(bluetoothctl show | grep "Powered:" | awk '{print $2}')
 if [[ "$powered" != "yes" ]]; then
-    echo '{"text": " off", "tooltip": "Bluetooth is off", "class": "off"}'
+    echo '{"text": "󰂲", "tooltip": "Bluetooth is off", "class": "off"}'
     exit
 fi
 
 # Get connected device names
 connected=$(bluetoothctl devices Connected | cut -d ' ' -f 3-)
 if [[ -n "$connected" ]]; then
-    echo '{"text": " '$connected'", "tooltip": "Connected to: '$connected'", "class": "connected"}'
+    echo '{"text": "󰂯 '$connected'", "tooltip": "Connected to: '$connected'", "class": "connected"}'
 else
-    echo '{"text": " on", "tooltip": "Bluetooth is on", "class": "on"}'
+    echo '{"text": "󰂯", "tooltip": "Bluetooth is on", "class": "on"}'
 fi
 
